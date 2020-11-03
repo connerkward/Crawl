cba9fb0a5a5ed6d3b3a7bd66d584e72eb9eb1837
Conner Ward <ckward@uci.edu>
2020-11-03 03:37:11 -0800

    - Updated USER_AGENT in config.ini - Changed to higher speed (lower politeness) in config.ini. CHANGE BEFORE NOV 4. - Restructured File into modules, scaper() is now the daddy function that calls all the other functions. - Added easy to change out tokenizers. - Added optional ability in launch.py for deleting previous temp files like "data.json", "URL_LOG.txt", and "frontier.shelve.db" at startup. - Added catching of empty "zero sized string" ValueError returns in download.py. - Added JSON saving of frequency data, but need someone to validate. - added img, war, apk, mat, thesis to is_valid() - Added live url count to worker.py - Added "event", "events", "img" to trap check in extract_new_links() - Made limited changes to is_valid() or extract_next_links() functions besides restructuring, unless otherwise noted in an inline comment

--- crawler/worker.py
+++ crawler/worker.py
@@ -23,7 +23,7 @@ class Worker(Thread):
             self.logger.info(
                 f"Downloaded {tbd_url}, status <{resp.status}>, "
                 f"using cache {self.config.cache_server}.")
-            self.logger.info(len(self.frontier.save))  # added live url count
+            self.logger.info("total URL's: " + str(len(self.frontier.save)))  # added live url count
             scraped_urls = scraper(tbd_url, resp)
             for scraped_url in scraped_urls:
                 self.frontier.add_url(scraped_url)
